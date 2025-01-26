aa2dec461d2c721302d044fc3efe86eaeb017e42
Henrique Ribeiro Siqueira <71899443+henriquersiqueiraa@users.noreply.github.com>
2025-01-26 12:10:22 -0300

    Add files via upload

---
+++ listas.js
@@ -0,0 +1,17 @@
+console.log("Trabalhando com listas"); // Trabalha com listas.
+
+const salvador = "Salvador";
+const saoPaulo = "São Paulo";
+const rioDeJaneiro = "Rio de Janeiro";
+const brasilia = "Brasília";
+const listaDeDestinos = new Array(
+    salvador,
+    saoPaulo,
+    rioDeJaneiro,
+    brasilia
+); // Cria uma lista.
+
+listaDeDestinos.push("Curitiba"); // Adiciona um item na lista.
+
+console.log("Destinos possíveis:");
+console.log(listaDeDestinos);
